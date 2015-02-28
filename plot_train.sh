cdd9d9af5b854435daa642d5838aa6352081cbd8
Victor Escorcia <escorciav@gmail.com>
2015-02-28 22:51:29 +0300

    Change name of script to generate plot

--- plot_train.sh
+++ plot_train.sh
@@ -3,7 +3,7 @@ PLOT_TOOL_PATH="../caffe/tools/extra/plot_training_log.py.example"
 
 if [ -z "$1"  ] || [ -z "$2" ]
 then
-  echo "Usage: train_plot.sh OUTPUT_PREFIX LOGFILE"
+  echo "Usage: plot_train.sh OUTPUT_PREFIX LOGFILE"
 else
   for i in 0 2 6
   do
